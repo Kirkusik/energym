3035a3a4e7f6b865947ba47e699c8b65daba13ec
Roman Marchenko <kirkusik@gmail.com>
2021-07-10 12:07:38 +0300

    filebase

---
+++ src/js/import/modules.js
@@ -0,0 +1,14 @@
+import "%modules%/header/header";
+
+import "%modules%/main-page-hero-block/main-page-hero-block";
+import "%modules%/about-us/about-us";
+import "%modules%/calendar/calendar";
+import "%modules%/club-card/club-card";
+import "%modules%/news/news";
+import "%modules%/single-news/single-news";
+import "%modules%/service-simple/service-simple";
+import "%modules%/service-complex/service-complex";
+import "%modules%/service-kids/service-kids";
+import "%modules%/trainers/trainers";
+
+import "%modules%/footer/footer";
