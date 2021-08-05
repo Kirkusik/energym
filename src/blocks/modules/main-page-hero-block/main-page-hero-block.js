be4773aced3866b00815f69650d415d9f2f20459
Roman Marchenko <kirkusik@gmail.com>
2021-08-05 12:38:12 +0300

    connect right script file

--- src/blocks/modules/main-page-hero-block/main-page-hero-block.js
+++ src/blocks/modules/main-page-hero-block/main-page-hero-block.js
@@ -1,9 +1,17 @@
 (function () {
-    document.querySelector('.hero__arrow').addEventListener('click', () => {
-        document.querySelector('.hero__info').classList.remove('hero__info--position');
-        document.querySelector('.hero__info').classList.add('hero__info--active');
-        document.querySelector('.hero__info-title').classList.add('hero__info-title--active');
-        document.querySelector('.hero__links-box').classList.add('hero__links-box--active');
-        document.querySelector('.hero__arrow').classList.add('hero__arrow--active');
-    });
+   document.querySelector(".hero__arrow").addEventListener("click", () => {
+      document
+         .querySelector(".hero__info")
+         .classList.remove("hero__info--position");
+      document.querySelector(".hero__info").classList.add("hero__info--active");
+      document
+         .querySelector(".hero__info-title")
+         .classList.add("hero__info-title--active");
+      document
+         .querySelector(".hero__links-box")
+         .classList.add("hero__links-box--active");
+      document
+         .querySelector(".hero__arrow")
+         .classList.add("hero__arrow--active");
+   });
 })();
