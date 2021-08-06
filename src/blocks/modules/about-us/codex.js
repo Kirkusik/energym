b1dd394267fbe2bf99d5a3725fb3629c9078f8db
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-08-06 19:30:25 +0300

    fix bugs, add function for links parsing
    -change the class name video icon
    -change the min text size from 15 to 16px
    -change the font size property in mq for text block
    -add href with video link to codex__video-link for js
    -add function, which parses the link in html and returns youtube video`s id

--- src/blocks/modules/about-us/codex.js
+++ src/blocks/modules/about-us/codex.js
@@ -8,24 +8,32 @@
 
     function setupVideo(video) {
         let link = video.querySelector('.codex__video-link');
-        let media = video.querySelector('.codex__video-cover');
         let button = video.querySelector('.codex__video-btn');
-        // take id in the embed code on YouTube
-        let id = 'ZF9S6Bc1wBI';
+        let videoLink = link.href;
+        link.removeAttribute('href');
+
+        // YouTube video id 
+        let id = getVideoId(videoLink);
+
+        // classes for show play btn and add an interactive cursor to the whole video
+        video.classList.add('codex__video-wrapper--enabled');
+        codexVideo.classList.add('codex__video-icon');
 
         // by clicking on the video, the entire link with the image is deleted, the video is placed instead
         video.addEventListener('click', () => {
             let iframe = createIframe(id);
             link.remove();
             button.remove();
-            codexVideo.classList.remove('codex__video--icon');
+            codexVideo.classList.remove('codex__video-icon');
             video.appendChild(iframe);
         });
-
-        // classes for remove/show play btn and add an interactive cursor to the whole video
-        video.classList.add('codex__video-wrapper--enabled');
-        codexVideo.classList.add('codex__video--icon');
     };
+
+    // get the youtube video id from the link url
+    function getVideoId(videoLink) {
+        let regExp = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+        return videoLink.match(regExp)[1];
+    }
 
     // create iframe element
     function createIframe(id) {
@@ -38,10 +46,11 @@
 
         return iframe;
     }
+
+
     // generate url for iframe by id + attr
     function generateURL(id) {
-        let query = '?rel=0&showinfo=0&autoplay=1';
-        return 'https://www.youtube.com/embed/' + id + query;
-
+        let playerAttr = '?rel=0&showinfo=0&autoplay=1';
+        return 'https://www.youtube.com/embed/' + id + playerAttr;
     }
 })();
