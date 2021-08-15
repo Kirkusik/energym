045e1d2bbe4d6ccc3cee74e649d25e5adfc8fbfe
LuckySerj <lukianchuk.s@gmail.com>
2021-08-15 09:59:41 +0300

    test commit

---
+++ src/blocks/modules/service-complex/service-complex.js
@@ -0,0 +1,8 @@
+const serviceComplex = {
+  list: document.querySelector(".services-menu"),
+  header: document.querySelectorAll(".services-complex__default"),
+  items: document.querySelectorAll(".services-menu__link"),
+  defaultItem: document.querySelector(".services-complex__default-item"),
+};
+
+function serviceMenuClick(params) {}
