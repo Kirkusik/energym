c83e9b5933251c42501168dccc0a21c58a800c1f
Дмитрий Соверченко <isoverchenko@gmail.com>
2021-09-02 20:42:20 +0300

    d

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -15,62 +15,62 @@ import yargs from "yargs";
 const sass = require("gulp-sass")(require("sass"));
 
 const argv = yargs.argv,
-   production = !!argv.production;
+  production = !!argv.production;
 
-gulp.task("styles", async() => {
-   return gulp
-      .src(paths.styles.src)
-      .pipe(gulpif(!production, sourcemaps.init()))
-      .pipe(plumber())
-      .pipe(sass())
-      .pipe(groupmedia())
-      .pipe(
-         gulpif(
-            production,
-            autoprefixer({
-               cascade: false,
-               grid: true,
-            })
-         )
+gulp.task("styles", async () => {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(gulpif(!production, sourcemaps.init()))
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(groupmedia())
+    .pipe(
+      gulpif(
+        production,
+        autoprefixer({
+          cascade: false,
+          grid: true,
+        })
       )
-      .pipe(
-         gulpif(
-            production,
-            mincss({
-               compatibility: "ie8",
-               level: {
-                  1: {
-                     specialComments: 0,
-                     removeEmpty: true,
-                     removeWhitespace: true,
-                  },
-                  2: {
-                     mergeMedia: true,
-                     removeEmpty: true,
-                     removeDuplicateFontRules: true,
-                     removeDuplicateMediaBlocks: true,
-                     removeDuplicateRules: true,
-                     removeUnusedAtRules: false,
-                  },
-               },
-            })
-         )
+    )
+    .pipe(
+      gulpif(
+        production,
+        mincss({
+          compatibility: "ie8",
+          level: {
+            1: {
+              specialComments: 0,
+              removeEmpty: true,
+              removeWhitespace: true,
+            },
+            2: {
+              mergeMedia: true,
+              removeEmpty: true,
+              removeDuplicateFontRules: true,
+              removeDuplicateMediaBlocks: true,
+              removeDuplicateRules: true,
+              removeUnusedAtRules: false,
+            },
+          },
+        })
       )
-      .pipe(
-         gulpif(
-            production,
-            rename({
-               suffix: ".min",
-            })
-         )
+    )
+    .pipe(
+      gulpif(
+        production,
+        rename({
+          suffix: ".min",
+        })
       )
-      .pipe(plumber.stop())
-      .pipe(gulpif(!production, sourcemaps.write("./maps/")))
-      .pipe(gulp.dest(paths.styles.dist))
-      .pipe(
-         debug({
-            title: "CSS files",
-         })
-      )
-      .pipe(browsersync.stream());
+    )
+    .pipe(plumber.stop())
+    .pipe(gulpif(!production, sourcemaps.write("./maps/")))
+    .pipe(gulp.dest(paths.styles.dist))
+    .pipe(
+      debug({
+        title: "CSS files",
+      })
+    )
+    .pipe(browsersync.stream());
 });
