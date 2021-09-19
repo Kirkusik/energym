8be16e509c10753c406edc1d96aacb71a8c045dd
Дмитрий Соверченко <isoverchenko@gmail.com>
2021-09-20 00:19:11 +0300

    add scripts to choose card and change html atfer it

--- src/blocks/modules/club-card/club-card.js
+++ src/blocks/modules/club-card/club-card.js
@@ -5,6 +5,11 @@ const cards = {
   darkBcground: document.querySelector(".cards-modal__bg-dark"),
   closeBtn: document.querySelector(".cards-modal__close-btn"),
   body: document.querySelector("body"),
+  menu: document.querySelector(".cards-modal__cards-wrapper"),
+  table: document.querySelectorAll(".service-table__value"),
+  secondItem: document.querySelectorAll(
+    ".service-table__value--compare-second"
+  ),
 };
 
 cards.compareBtn.forEach((el) => {
@@ -18,11 +23,25 @@ function showModal() {
   cards.body.classList.add("show-modal");
 }
 
-if (cards.closeBtn) {
-  cards.closeBtn.addEventListener("click", () => {
-    cards.modal.classList.remove("cards-modal--show");
-    cards.container.classList.remove("cards__container--modal-open");
-    cards.darkBcground.classList.remove("cards-modal__bg-dark--show");
-    cards.body.classList.remove("show-modal");
-  });
+function closeModal() {
+  cards.modal.classList.remove("cards-modal--show");
+  cards.container.classList.remove("cards__container--modal-open");
+  cards.darkBcground.classList.remove("cards-modal__bg-dark--show");
+  cards.body.classList.remove("show-modal");
 }
+
+if (cards.closeBtn) {
+  cards.closeBtn.addEventListener("click", closeModal);
+}
+
+cards.menu.addEventListener("click", function (event) {
+  closeModal();
+  cards.secondItem.forEach((el) => {
+    el.classList.remove("service-table__value--compare-second");
+  });
+  cards.table.forEach((el) => {
+    el.classList.add("service-table__value--compare");
+  });
+  console.dir(event.target);
+  console.log(event.target.src);
+});
