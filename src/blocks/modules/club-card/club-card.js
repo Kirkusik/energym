b14263e92a3feb7282ccfa9f8d8c24ff9d4d5c6d
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-10-02 00:05:13 +0300

    fix list of cards data from 1 ul to separate parts

--- src/blocks/modules/club-card/club-card.js
+++ src/blocks/modules/club-card/club-card.js
@@ -1,61 +1,63 @@
 const cards = {
-  compareBtn: document.querySelectorAll(".compare-cards__link"),
-  modal: document.querySelector(".cards-modal"),
-  container: document.querySelector(".cards__container"),
-  darkBcground: document.querySelector(".cards-modal__bg-dark"),
-  closeBtn: document.querySelector(".cards-modal__close-btn"),
-  body: document.querySelector("body"),
-  menu: document.querySelector(".cards-modal__cards-wrapper"),
-  table: document.querySelectorAll(".service-table__value"),
-  secondItem: document.querySelectorAll(
-    ".service-table__value--compare-second"
-  ),
+    compareBtn: document.querySelectorAll(".compare-cards__link"),
+    modal: document.querySelector(".cards-modal"),
+    container: document.querySelector(".cards__container"),
+    darkBcground: document.querySelector(".cards-modal__bg-dark"),
+    closeBtn: document.querySelector(".cards-modal__close-btn"),
+    body: document.querySelector("body"),
+    menu: document.querySelector(".cards-modal__cards-wrapper"),
+    table: document.querySelectorAll(".service-table__value"),
+    secondItem: document.querySelectorAll(
+        ".service-table__value--compare-second"
+    ),
 };
 
 cards.compareBtn.forEach((el) => {
-  el.addEventListener("click", showModal);
+    el.addEventListener("click", showModal);
 });
 
 function showModal() {
-  cards.modal.classList.add("cards-modal--show");
-  cards.container.classList.add("cards__container--modal-open");
-  cards.darkBcground.classList.add("cards-modal__bg-dark--show");
-  cards.body.classList.add("show-modal");
+    cards.modal.classList.add("cards-modal--show");
+    cards.container.classList.add("cards__container--modal-open");
+    cards.darkBcground.classList.add("cards-modal__bg-dark--show");
+    cards.body.classList.add("show-modal");
 }
 
 function closeModal() {
-  cards.modal.classList.remove("cards-modal--show");
-  cards.container.classList.remove("cards__container--modal-open");
-  cards.darkBcground.classList.remove("cards-modal__bg-dark--show");
-  cards.body.classList.remove("show-modal");
+    cards.modal.classList.remove("cards-modal--show");
+    cards.container.classList.remove("cards__container--modal-open");
+    cards.darkBcground.classList.remove("cards-modal__bg-dark--show");
+    cards.body.classList.remove("show-modal");
 }
 
 if (cards.closeBtn) {
-  cards.closeBtn.addEventListener("click", closeModal);
+    cards.closeBtn.addEventListener("click", closeModal);
 }
 
+console.log(cards.menu);
 cards.menu.addEventListener("click", function (event) {
-  closeModal();
-  cards.secondItem.forEach((el) => {
-    el.classList.remove("service-table__value--compare-second");
-  });
-  cards.table.forEach((el) => {
-    el.classList.add("service-table__value--compare");
-  });
+    console.log(event.target);
+    closeModal();
+    cards.secondItem.forEach((el) => {
+        el.classList.remove("service-table__value--compare-second");
+    });
+    cards.table.forEach((el) => {
+        el.classList.add("service-table__value--compare");
+    });
 
-document.querySelector(".compare-cards__link--btn").classList.add("invisible");
+    document.querySelector(".compare-cards__link--btn").classList.add("invisible");
 
-  document
-    .querySelector(".compare-cards")
-    .children[+event.target.getAttribute("data-tab-title")].classList.add(
-      "active"
+    document
+        .querySelector(".compare-cards")
+        .children[+event.target.getAttribute("data-tab-title")].classList.add(
+            "active"
+        );
+    console.dir(event.target);
+    console.log(event.target.src);
+    console.log(event.target.getAttribute("data-tab-title"));
+    console.log(
+        document.querySelector(".compare-cards").children[
+        +event.target.getAttribute("data-tab-title")
+        ]
     );
-  // console.dir(event.target);
-  // console.log(event.target.src);
-  console.log(event.target.getAttribute("data-tab-title"));
-  console.log(
-    document.querySelector(".compare-cards").children[
-      +event.target.getAttribute("data-tab-title")
-    ]
-  );
 });
