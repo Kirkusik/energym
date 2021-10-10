0dacf28cecd358f98271795eec528c6d248b09b2
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-10-11 01:17:03 +0300

    fix changing images on cards

--- src/blocks/modules/club-card/club-card.js
+++ src/blocks/modules/club-card/club-card.js
@@ -19,7 +19,7 @@ const cards = {
 const compareCards = document.querySelector(".compare-cards");
 const firstCardImg = compareCards.querySelector('.compare-cards__link--first img');
 const secondCardImg = compareCards.querySelector('.compare-cards__link--second img');
-
+let cardId;
 
 function showModal() {
     cards.modal.classList.add("cards-modal--show");
@@ -39,44 +39,36 @@ if (cards.closeBtn) {
     cards.closeBtn.addEventListener("click", closeModal);
 }
 
+cards.menu.addEventListener("click", function (event) {
+    closeModal();
+    changeCardImg(cardId, event)
 
+});
 
-function changeCardImg(cardId) {
-    cards.menu.addEventListener("click", function (event) {
-        closeModal();
+function changeCardImg(cardId, event) {
 
-        // cards.table.forEach((el) => {
-        //     el.classList.add("service-table__value--compare");
-        // });
+    if (cardId === 1) {
+        firstCardImg.setAttribute('src', event.target.getAttribute("src"));
+        console.log(firstCardImg);
+    } else if (cardId === 2) {
+        document.querySelector(".compare-cards__btn").classList.add("invisible");
+        document.querySelector(".compare-cards__second-pic").classList.add("active");
 
-        let cardImgSrc;
-        if (cardId === 1) {
-            cardImgSrc = event.target.getAttribute("src");
-            firstCardImg.setAttribute('src', cardImgSrc);
-            console.log(firstCardImg);
-        } else if (cardId === 2) {
-            document.querySelector(".compare-cards__btn").classList.add("invisible");
-            document.querySelector(".compare-cards__second-pic").classList.add("active");
+        cards.secondItem.forEach((el) => {
+            el.classList.remove("service-table__value--compare-second");
+        });
 
-            cards.secondItem.forEach((el) => {
-                el.classList.remove("service-table__value--compare-second");
-            });
-
-            cardImgSrc = event.target.getAttribute("src");
-            secondCardImg.setAttribute('src', cardImgSrc);
-            console.log(secondCardImg);
-        }
-
-    });
+        secondCardImg.setAttribute('src', event.target.getAttribute("src"));
+        console.log(secondCardImg);
+    }
 }
-
 
 compareCards.addEventListener("click", (e) => {
     showModal();
     if (e.target.closest('.compare-cards__link--first')) {
-        changeCardImg(1);
+        cardId = 1;
     }
     if (e.target.closest('.compare-cards__link--second')) {
-        changeCardImg(2);
+        cardId = 2;
     }
 })
